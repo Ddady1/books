1218b1385e50ed3b9fcf8de84ac7704c69fafc84
Ddady1 <ddady1@gmail.com>
2024-03-31 15:57:40 +0300

    search

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ driver.get(f'https://www.booknet.co.il/%D7%97%D7%99%D7%A4%D7%95%D7%A9?q={booknam
 
 # עברית
 #driver.get(f'https://www.e-vrit.co.il/Search/{bookname}')
+
+price = driver.find_element(By.TAG_NAME, 'ins').text
+ext = driver.find_elements(By.XPATH, '//span[@class="cbar_sale_price"]')
+price
+ext[0].text
