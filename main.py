0462214407b29b600a69a69bb6d8716a1668a133
Ddady1 <ddady1@gmail.com>
2024-04-01 16:32:34 +0300

    con

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+#driver.get('https://www.booknet.co.il/')
 bookname = input('Please enter books name:')
+
 # צומת ספרים
 driver.get(f'https://www.booknet.co.il/%D7%97%D7%99%D7%A4%D7%95%D7%A9?q={bookname}')
 
@@ -16,6 +18,17 @@ driver.get(f'https://www.booknet.co.il/%D7%97%D7%99%D7%A4%D7%95%D7%A9?q={booknam
 #driver.get(f'https://www.e-vrit.co.il/Search/{bookname}')
 
 price = driver.find_element(By.TAG_NAME, 'ins').text
-ext = driver.find_elements(By.XPATH, '//span[@class="cbar_sale_price"]')
-print(price)
-print(ext[0].text)
+prices = driver.find_elements(By.TAG_NAME, 'ins')
+bookid = driver.find_elements(By.XPATH, '//div[contains(@class, "data-prodid")]')
+print(type(bookid))
+#ext = driver.find_elements(By.XPATH, '//span[@class="cbar_sale_price"]')
+ext = driver.find_elements(By.XPATH, '//div[@class="products product-cube col-md-2"]')
+#price
+i = 0
+for item in prices:
+    print(item.text)
+    #print(bookid[i].text)
+    i += 1
+print(len(bookid))
+
+#ext[0]
