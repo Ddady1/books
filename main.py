380e466defef226461361e286dd1c37199028f2b
Ddady1 <ddady1@gmail.com>
2024-04-17 12:38:30 +0300

    new way and notepad

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def tzomet(driver):
     authors = driver.find_elements(By.XPATH, '//a[@class="book-below-title product-author"]')
     images = driver.find_elements(By.XPATH, '//img[@class="img-responsive img-lazy-load"]')
     # azal = driver.find
+    in_stock = None
+    is_printed = True
+    is_digital = False
+    in_basket = 'הוסף לסל'
+    price_str = 'מחיר מכירה'
     books_image = []
     for image in images:
         books_image.append(image.get_attribute("src"))
@@ -19,20 +24,35 @@ def tzomet(driver):
     exc = driver.find_elements(By.XPATH, '//button/span[@class="sr-only"]')
     # print(exc[0].text)
     for book in books:
-        text1 = book.text
-        text2 = text1.split('\n')
-        print(text2)
-        if exc[looper].text in text2:
-            text2.remove(exc[looper].text)
-        print(text2)
+        raw_details = book.text
+        book_id = book.get_attribute("data-prodid")
+        book_details = raw_details.split('\n')
+        print(book_details)
+        if exc[looper].text in book_details:
+            book_details.remove(exc[looper].text)
+        if price_str in book_details:
+            book_details.remove(price_str)
+        print(book_details)
+        if len(book_details) >= 5:
+            book_details.pop(0)
+        book_details.append(books_image[looper])
+        print(book_details)
+        if in_basket in book_details:
+            in_stock = True
+        else:
+            in_stock = False
+        book_details.pop(3)
+        book_details.insert(3, in_stock)
+        book_details.append(is_printed)
+        book_details.insert(0, book_id)
 
         i = 0
-        for item in text2:
+        for item in book_details:
             print(f'{i} - {item}')
             i += 1
         if looper < len(exc) - 1:
             looper += 1
-            print(looper)
+            # print(looper)
     '''for book in books:
         # print(item.text)
         book_details = []
